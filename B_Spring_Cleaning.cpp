744174bffe81bc341e8eace8ff6668c880a8c238
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2026-02-16 10:52:41 +0600

    Add files via upload

---
+++ B_Spring_Cleaning.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int x,y;
+    cin >> x >> y;
+    int total_time = (x*30) + (y*60);
+    cout << total_time << endl;
+    return 0;
+}
